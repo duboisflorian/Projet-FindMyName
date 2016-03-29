62067e4818e4e7e3aaa349b5e2c167dac3922f48
duboisflorian <flo.dub305@gmail.com>
2016-03-29 15:06:11 +0200

    feat : css 2.0 debut

--- Code/Pandore/src/Pandore/wwwroot/app/acc_app.ts
+++ Code/Pandore/src/Pandore/wwwroot/app/acc_app.ts
@@ -7,9 +7,9 @@ import {Top} from './top';
 @Component({
     selector: 'acc-app',
     template: `
-<top class="container" style= "display: block" > Loading...</top>
-<ami-app class="container" style= "display: block" > Loading...</ami-app>
-<footer class="container" style= "display: block" > Loading...</footer>
+<top> Loading...</top>
+<ami-app> Loading...</ami-app>
+<footer> Loading...</footer>
 
 `, directives: [Top, AmiApp, Footer]
 })
