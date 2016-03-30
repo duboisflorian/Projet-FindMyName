7771a98af8b6720527711b3406d16b763098083c
LionelHr <lionel.harimanitra@hotmail.fr>
2016-03-30 21:15:07 +0200

    Ajout d'une page de connexion
    La page de connexion se situe entre l'index.html et co.html

---
+++ Code/Pandore/src/Pandore/wwwroot/app/traitement_connexion.ts
@@ -0,0 +1,23 @@
+﻿function connexion() {
+
+    $.getJSON('../fichier/connexion.json', function (data) {
+
+        var login = document.getElementById("login").value;
+        var mdp = document.getElementById("password").value;
+        alert(login);
+        alert(mdp);
+
+        $.each(data, function (key, val) {
+            if ((val.login == login) && (val.mdp == mdp)) {
+                alert("Vous êtes connecté !");
+                document.location.href = "co.html";
+                return false;                
+            }
+            else {
+                alert("Erreur de connection !");
+                //return false;
+            }
+            //$("div").append(" "+ val +"<br/>");
+        });
+    });
+}
