c54a6e5794dea74a7f9390270a1ad88695c536de
Tinorus <tinorus91@gmail.com>
2016-03-31 10:40:15 +0200

    Feat: Html ami-app

--- Code/Pandore/src/Pandore/wwwroot/app/ami_app.ts
+++ Code/Pandore/src/Pandore/wwwroot/app/ami_app.ts
@@ -7,33 +7,34 @@ import {AmiList} from './ami_list';
 @Component({
     selector: 'ami-app',
     template: `
-    <div class="container core">
-          <div class="row">  
-              <ul class="nav nav-button col-xs-6 col-sm-4 col-lg-3 col-md-3">
-                 <li class="active"><a href="#"><span class="text">Ami</span></a></li>
-                 <li><a href="#"><span class="text">Online</span></a></li>
-              </ul>  
-          </div>
-          <div class="row">
-              <div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 sidebar">
-                  
-                  <ul class="nav nav-sidebar">
-                      <li class="active">
-                          <a href="#">
-                            <img class="portrait" src="fichier/Portrait.png" /><span class="nom"> {{joueur}}</span>
-                        </a>
-                    </li>
-                 <ami-list [amis]="amis"></ami-list>
-               </ul>
-            </div>
-        </div>
-        <div class="row">
-                  <div class="nav nav-ajouter col-xs-6 col-sm-4 col-lg-3 col-md-3">
-                    <ami-form (newFriend)="addFriend($event)"></ami-form>
-            </div>
-        </div>
-    </div>
+   <div class="core">
+          <div class="row" style=" margin-right:0px;">
+              <div class="list-ami col-xs-6 col-sm-4 col-lg-3 col-md-3">
+                  <ul class="nav nav-button">
+                      <li class="active"><a href="#"><span class="text">Ami</span></a></li>
+                      <li><a href="#"><span class="text">Online</span></a></li>
+                  </ul>
 
+                  <div class="sidebar">
+
+                      <ul class="nav nav-sidebar">
+                          <li class="active">
+                              <a href="#">
+                                  <img class="portrait" src="fichier/Portrait.png" /><span class="nom"> Florian</span>
+                              </a>
+                          </li>
+                        
+                        <ami-list [amis]="amis"></ami-list>
+
+                      </ul>
+                  </div>
+                  <div class="nav nav-ajouter">
+                         <a href="" onclick=""><span class="text">Ajouter un ami</span></a>
+                            <ami-form (newFriend)="addFriend($event)"></ami-form>
+                  </div>
+              </div>
+          </div>  
+       </div>
 `, directives: [AmiForm, AmiList]
 })
 export class AmiApp {
--- Code/Pandore/src/Pandore/wwwroot/app/ami_list.ts
+++ Code/Pandore/src/Pandore/wwwroot/app/ami_list.ts
@@ -4,8 +4,8 @@ import {Ami} from './ami';
 @Component({
     selector: 'ami-list',
     template: `
-    <li><form class="form"><input id="recherche" class="form-control form-recherche" placeholder="Rachercher un ami" required="" type="text"></form></li>
-    <li *ngFor="#ami of amis"><a href="{{ami.id}}"><img class="portrait" src="{{ami.photo}}" (click)="Detail(ami.id)" /><span class="nom"> {{ami.text}} </span></a></li>
+    <li><form class="form"><input id="recherche" class=" form-recherche" placeholder="Rachercher un ami" required="" type="text"></form></li>
+    <li class="ligne-ami" *ngFor="#ami of amis"><a href="{{ami.id}}"><img class="portrait" src="{{ami.photo}}" (click)="Detail(ami.id)" /><span class="nom"> {{ami.text}} </span></a></li>
 `
 })
 export class AmiList {
