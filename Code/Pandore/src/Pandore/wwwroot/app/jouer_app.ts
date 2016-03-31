18b73f811f364c08eba408f11137638eabd032bf
Eldii <m1105.108.baba@gmail.com>
2016-03-31 12:32:17 +0200

    feat : fonction jouer terminé

--- Code/Pandore/src/Pandore/wwwroot/app/jouer_app.ts
+++ Code/Pandore/src/Pandore/wwwroot/app/jouer_app.ts
@@ -2,6 +2,7 @@
 import {Footer} from './footer';
 import {Top} from './top';
 import {Todo} from './todo';
+import {Joueurs} from './joueurs';
 import {TodoList} from './todo_list';
 import {TodoForm} from './todo_form';
 
@@ -12,7 +13,7 @@ import {TodoForm} from './todo_form';
 <top> Loading...</top>
 <div class="container core" style="color:white;">
     <div id="gauche" style="width:20%;float:left;">
-    <span style="text-align: center;">{{remaining}} of {{joueurs.length}} remaining</span>
+    <span style="text-align: center;">{{remaining}} of {{nbjoueurs}} remaining</span>
     <h2>Liste des joueurs</h2>
     <todo-list [todos]="todos"></todo-list>
 </div>
@@ -28,14 +29,44 @@ export class JouerApp {
     todos: Todo[] = [
     ];
 
-    joueurs: Joueurs[] = [
+    /*joueurs: Joueurs[] = [
         { text: "Rabiot" },
         { text: "Zlatan" },
         { text: "Cavani" },
         { text: "Trapp" },
-    ];
+    ];*/
+
+    static nbj: string;
+    nbjoueurs: string;
+    constructor() {
+
+        var xobj = new XMLHttpRequest();
+
+        xobj.overrideMimeType("application/json");
+
+        xobj.open('GET', './fichier/equipe.json', true); // Replace 'my_data' with the path to your file
+
+        xobj.onreadystatechange = function () {
+
+            if (xobj.readyState == 4 && xobj.status == 200) {
+
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                //alert(xobj.responseText);
+                var data = eval("(" + xobj.responseText + ")");
+
+                JouerApp.nbj = data.nb;
+
+            }
+        };
+        xobj.send(null);
+
+
+
+    }
+
 
     get remaining() {
+        this.nbjoueurs = JouerApp.nbj;
         return this.todos.reduce((count: number, todo: Todo) => count + +!todo.done, 0);
     }
 
