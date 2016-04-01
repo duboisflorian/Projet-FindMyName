a5cb266bbe7dbbe1b107c4482396427f4a1c60ff
Eldii <m1105.108.baba@gmail.com>
2016-04-01 11:31:08 +0200

    feat : themes aléatoire

--- Code/Pandore/src/Pandore/wwwroot/app/choix_app.ts
+++ Code/Pandore/src/Pandore/wwwroot/app/choix_app.ts
@@ -39,15 +39,17 @@ export class ChoixApp {
     todos: Todo[] = [
     ];
 
-    theme: Theme[] = [
+    /*theme: Theme[] = [
         { text: "PSG",id:"1",photo:"./fichier/psg.jpg", done: false},
         { text: "OL", id: "2", photo: "./fichier/ol.jpg", done: false},
         { text: "REAL", id: "3", photo: "./fichier/real.jpg", done: false },
         { text: "Barca", id: "4", photo: "./fichier/barca.jpg", done: false },
         { text: "arsenal", id: "5", photo: "./fichier/arsenal.jpg", done: false },
         { text: "city", id: "6", photo: "./fichier/city.jpg", done: false },
-    ];
+    ];*/
 
+    theme: Theme[] = [
+    ];
     static themes: Theme[] = [
     ];
     constructor() {
@@ -56,7 +58,7 @@ export class ChoixApp {
 
         xobj.overrideMimeType("application/json");
 
-        xobj.open('GET', './fichier/equipe.json', true); // Replace 'my_data' with the path to your file
+        xobj.open('GET', './fichier/themes.json', true); // Replace 'my_data' with the path to your file
 
         xobj.onreadystatechange = function () {
 
@@ -65,18 +67,56 @@ export class ChoixApp {
                 // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
                 //alert(xobj.responseText);
                 var data = eval("(" + xobj.responseText + ")");
+                var c = 0;
+                var tab_id: number[];
+                while (c < 4) {
+                    alert(c);
+                    alert(i);
+                    var i = Math.floor(Math.random() * 5) + 1;
+                    ChoixApp.themes.push({ text: data.themes[i].text, id: data.themes[i].id, photo: data.themes[i].photo, done: true });
+                    c++;
+                    
 
-                for (var i = 0; i < data.joueurs.length; i++) {
-
-                    TodoForm.joueurs.push({ text: data.joueurs[i].text, done: data.joueurs[i].done });
+                    /*for (var a = 0; a < 4; a++) {
+                        if (tab_id[a] != i) {
+                            ChoixApp.themes.push({ text: data.themes[i].text, id: data.themes[i].id, photo: data.themes[i].photo, done: true });
+                            c++;
+                            tab_id.push(i);
+                        }
+                    }*/
                 }
+                /*for (var i = 0; i < data.themes.length; i++) {
 
+                    ChoixApp.themes.push({ text: data.themes[i].text, id: data.themes[i].id, photo: data.themes[i].photo, done: data.themes[i].done });
+                }*/
             }
         };
         xobj.send(null);
     }
 
+    /* aleatoire() {
+         var c=0;
+         while (c < 4) {
+             var i = Math.floor(Math.random() * ChoixApp.themes.length) + 1;
+             this.theme.push(ChoixApp.themes[i]);
+             c++;
+         }
+     }*/
+
+    aleatoire() {
+        var c = 0;
+        while (c < 4) {
+            var i = Math.floor(Math.random() * 5) + 1;
+            if (ChoixApp.themes[i].done == false) {
+                this.theme.push(ChoixApp.themes[i]);
+                ChoixApp.themes[i].done = true;
+                c++;
+            }
+        }
+    }
+
     get remaining() {
+        this.theme = ChoixApp.themes;
         return this.todos.reduce((count: number, todo: Todo) => count + +!todo.done, 0);
     }
 
