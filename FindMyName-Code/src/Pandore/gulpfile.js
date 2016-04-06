d33cc7835319754714d38b38433405db06163344
Tinorus <tinorus91@gmail.com>
2016-04-06 21:59:39 +0200

    Feat: Less via Gulp

--- FindMyName-Code/src/Pandore/gulpfile.js
+++ FindMyName-Code/src/Pandore/gulpfile.js
@@ -1,26 +1,16 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
+﻿/// <binding BeforeBuild='css' AfterBuild='default' />
 
 var gulp = require('gulp');
 
-gulp.task('moveToLibs', function (done) {
-    gulp.src([
-      'node_modules/angular2/bundles/js',
-      'node_modules/angular2/bundles/angular2.*.js*',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/angular2/bundles/http.*.js*',
-      'node_modules/angular2/bundles/router.*.js*',
-      'node_modules/es6-shim/es6-shim.min.js*',
-      'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
-      'node_modules/systemjs/dist/*.*',
-      'node_modules/jquery/dist/jquery.*js',
-      'node_modules/bootstrap/dist/js/bootstrap*.js',
-      'node_modules/rxjs/bundles/Rx.js'
-    ]).pipe(gulp.dest('./wwwroot/libs/'));
+var plugins = require('gulp-load-plugins')();
 
-    gulp.src([
-      'node_modules/bootstrap/dist/css/bootstrap.css'
-    ]).pipe(gulp.dest('./wwwroot/libs/css'));
+gulp.task('css', function () {
+    return gulp.src('./wwwroot/libs/less/style.less')
+    .pipe(plugins.less())
+   .pipe(gulp.dest('./wwwroot/libs/css'));
 });
+
+gulp.task('build', ['css']);
+
+// Tâche par défaut
+gulp.task('default', ['build']);
