429fdaf772268202a76675a440bd49a04b357d4d
duboisflorian <flo.dub305@gmail.com>
2016-04-06 09:57:59 +0200

    feat : debut pop ajout d'ami

---
+++ FindMyName-Code/src/Pandore/wwwroot/app/mock-utilisateurs.ts
@@ -0,0 +1,15 @@
+﻿import { Utilisateur } from './utilisateur';
+
+export var UTILISATEURS: Utilisateur[] = [
+    { "id": 1, "name": "Florian", "photo": "fichier/logo.jpg" },
+    { "id": 2, "name": "Yani", "photo": "fichier/logo.jpg" },
+    { "id": 3, "name": "maxime", "photo": "fichier/logo.jpg" },
+    { "id": 4, "name": "lionel", "photo": "fichier/logo.jpg" },
+    { "id": 5, "name": "diego", "photo": "fichier/logo.jpg" },
+];
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
