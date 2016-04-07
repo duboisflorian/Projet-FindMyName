944ee405d07166154376d6fb5330d843583be987
duboisflorian <flo.dub305@gmail.com>
2016-04-07 12:58:58 +0200

    feat : detail
    creation données parties

---
+++ FindMyName-Code/src/Pandore/wwwroot/app/mock-parti.ts
@@ -0,0 +1,30 @@
+﻿import { Partie } from './parti';
+import { Manche } from './manche';
+
+export var PARTIES: Partie[] = [
+    {
+        "id_partie": 1,
+        "id_j1": 6,
+        "id_j2": 2,
+        "s1": 1,
+        "s2": 2,
+        "manche": [
+            { "id_theme": 1, "s1": 10, "s2": 20},
+            { "id_theme": 2, "s1": 15, "s2": 7},
+            { "id_theme": 3, "s1": 10, "s2": 15}
+        ]
+    },
+    {
+        "id_partie": 1,
+        "id_j1": 6,
+        "id_j2": 3,
+        "s1": 1,
+        "s2": 3,
+        "manche": [
+            { "id_theme": 1, "s1": 10, "s2": 20 },
+            { "id_theme": 2, "s1": 15, "s2": 7 },
+            { "id_theme": 3, "s1": 10, "s2": 15 },
+            { "id_theme": 4, "s1": 10, "s2": 15 }
+        ]
+    }
+];
