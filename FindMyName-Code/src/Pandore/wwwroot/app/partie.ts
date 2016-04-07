25b82372ea4206112e17296f2bfcbd55156f0e15
duboisflorian <flo.dub305@gmail.com>
2016-04-07 11:55:50 +0200

    feat : creation d'objet
    creation manche et partie .ts

---
+++ FindMyName-Code/src/Pandore/wwwroot/app/partie.ts
@@ -0,0 +1,10 @@
+﻿import { Manche } from './manche';
+
+export class Partie {
+    id_partie: number;
+    id_j1: number;
+    id_j2: number; 
+    s1: number;
+    s2: number;
+    manche: Manche[];
+}
