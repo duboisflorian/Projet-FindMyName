72cc591045d123a9679cccbd2f2ccbffc4c026bb
duboisflorian <flo.dub305@gmail.com>
2016-04-08 11:36:41 +0200

    feat : resolution bug

--- FindMyName-Code/src/Pandore/wwwroot/app/jouer.service.ts
+++ FindMyName-Code/src/Pandore/wwwroot/app/jouer.service.ts
@@ -53,8 +53,3 @@ export class JouerService {
         return Reponses[i].reponses.length;
     }
 }
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
