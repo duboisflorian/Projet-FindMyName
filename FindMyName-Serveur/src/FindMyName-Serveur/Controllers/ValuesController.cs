21419aaaebae473f8392fbf2938bb61a49a53dde
duboisflorian <flo.dub305@gmail.com>
2016-04-28 12:45:45 +0200

    feat

--- FindMyName-Serveur/src/FindMyName-Serveur/Controllers/ValuesController.cs
+++ FindMyName-Serveur/src/FindMyName-Serveur/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Cors;
+using FindMyName_Serveur.Models;
 
 namespace FindMyName_Serveur.Controllers
 {
@@ -21,7 +22,7 @@ namespace FindMyName_Serveur.Controllers
         [HttpGet("{id}")]
         public Theme Get(int id)
         {
-            return new Theme("PSG", "1", "./fichier/psg.jpg", false);
+            return new Theme("PSG", 1, "./fichier/psg.jpg", false);
         }
 
         // POST api/values
@@ -44,27 +45,3 @@ namespace FindMyName_Serveur.Controllers
     }
 }
 
-public class Theme
-{
-    public string text { get; set; }
-    public string id { get; set; }
-    public string photo { get; set; }
-    public bool done { get; set; }
-
-    public Theme(string t,string i,string p , bool d)
-    {
-        this.text =t;
-        this.id =i;
-        this.photo =p;
-        this.done =d;
-    }
-
-    public Theme()
-    {
-    }
-}
-
-public class ListeTheme
-{
-    public List<Theme> themes { get; set; }
-}
