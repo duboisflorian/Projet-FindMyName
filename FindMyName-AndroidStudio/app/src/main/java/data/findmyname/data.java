7d8cb3486e9e17dba6a641f65e303da313385d38
LionelHr <lionel.harimanitra@hotmail.fr>
2016-05-04 23:16:14 +0200

    Début traitement de la page de game

--- FindMyName-AndroidStudio/app/src/main/java/data/findmyname/data.java
+++ FindMyName-AndroidStudio/app/src/main/java/data/findmyname/data.java
@@ -19,6 +19,25 @@ public class data extends SQLiteOpenHelper{
 
     public void onCreate(SQLiteDatabase db)
     {
+        db.execSQL("CREATE TABLE themeol (id INTEGER PRIMARY KEY AUTOINCREMENT, reponse TEXT);");
+        Cursor result2 = db.rawQuery("SELECT * FROM themeol", null);
+        int nbcol2 = result2.getColumnCount();
+
+        Log.i("ENI","Nombre de colonnes " + nbcol2);
+
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Beauvue')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Labidi')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Lacazette')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Mvuemba')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Fekir')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Jallet')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Rose')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Darder')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Malbranque')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Diakhaby')");
+        db.execSQL("INSERT INTO themeol(reponse) VALUES ('Comet')");
+
+
         db.execSQL("CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, pseudo TEXT, mdp TEXT, image TEXT, mail TEXT, pays TEXT, meilleurscore INT);");
 
         Cursor result = db.rawQuery("SELECT * FROM user", null);
--- FindMyName-AndroidStudio/app/src/main/java/game/findmyname/game.java
+++ FindMyName-AndroidStudio/app/src/main/java/game/findmyname/game.java
@@ -1,7 +1,18 @@
 package game.findmyname;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+import data.findmyname.data;
 
 /**
  * Created by lucar_000 on 04/05/2016.
@@ -12,6 +23,41 @@ public class game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+
+        data databaseFilm = new data (getBaseContext(), "dbuser.db", null, 1);
+        SQLiteDatabase db = databaseFilm.getReadableDatabase();
+        db.setLocale(Locale.FRENCH);
+
+        final Cursor result = db.rawQuery("SELECT * FROM themeol",null);
+
+
+
+        Button btnValider = (Button) findViewById(R.id.btValider);
+        btnValider.setOnClickListener(new Button.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                EditText reponse = (EditText) findViewById(R.id.reponse);
+                String strreponse = reponse.getText().toString();
+
+                Log.i("ENI","Reponse proposée " +strreponse);
+
+                result.moveToFirst();
+                while (!result.isAfterLast())
+                {
+                    String text= result.getString(1);
+                    Log.i("ENI","Text : "+ text);
+
+                    if(strreponse.equals(text))
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(),"Bien jouer !",Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+
+                    result.moveToNext();
+                }
+            }
+        });
 
     }
 }
