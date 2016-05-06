e03fc622d12df3bda5b835ac0ee4c5abdebc2cbb
LionelHr <lionel.harimanitra@hotmail.fr>
2016-05-06 13:11:08 +0200

    Update Traitement inscirption

--- FindMyName-AndroidStudio/app/src/main/java/game/findmyname/inscription.java
+++ FindMyName-AndroidStudio/app/src/main/java/game/findmyname/inscription.java
@@ -1,5 +1,7 @@
 package game.findmyname;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import data.findmyname.data;
 import java.util.Locale;
 
@@ -29,8 +33,8 @@ public class inscription extends AppCompatActivity {
         btinscription.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
-                data inscriptiondb = new data(getBaseContext(), "dbmot.db", null, 1);
+
+                data inscriptiondb = new data(getBaseContext(), "dbuser.db", null, 1);
                 final SQLiteDatabase db = inscriptiondb.getWritableDatabase();
                 db.setLocale(Locale.FRENCH);
 
@@ -61,6 +65,40 @@ public class inscription extends AppCompatActivity {
                 String strPays = pays.getSelectedItem().toString();
 
                 Log.i("ENI","Pays : "+strPays);
+
+                db.execSQL("INSERT INTO user(pseudo,mdp,image,mail,pays,meilleurscore) VALUES('"+strpseudo+"','"+strmdp+"','logo.jpg','"+strmail+"','"+strPays+"',0)");
+
+                Cursor result = db.rawQuery("SELECT * FROM user",null);
+
+                result.moveToFirst();
+                while (!result.isAfterLast())
+                {
+                    String p = result.getString(1);
+                    Log.i("ENI","pseudo : "+ p);
+
+                    String m = result.getString(2);
+                    Log.i("ENI","mdp : "+ m);
+
+                    String i = result.getString(3);
+                    Log.i("ENI","image : "+ i);
+
+                    String mail = result.getString(4);
+                    Log.i("ENI","mail : "+ mail);
+
+                    String pa = result.getString(5);
+                    Log.i("ENI","pays : "+ pa);
+
+                    String meilScore = result.getString(6);
+                    Log.i("ENI","Meilleur Score : "+ meilScore);
+
+                    result.moveToNext();
+                }
+
+                Toast toast = Toast.makeText(getApplicationContext(),"Inscription réussi ",Toast.LENGTH_SHORT);
+                toast.show();
+
+                Intent intent = new Intent(inscription.this,MainActivity.class);
+                startActivity(intent);
 
             }
         });
