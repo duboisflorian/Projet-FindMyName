553562acd3f4e8308b5bb8651fc4a339e37793b5
LionelHr <lionel.harimanitra@hotmail.fr>
2016-05-07 01:33:01 +0200

    Début traitement Profil

--- FindMyName-AndroidStudio/app/src/main/java/game/findmyname/acceuil.java
+++ FindMyName-AndroidStudio/app/src/main/java/game/findmyname/acceuil.java
@@ -3,6 +3,7 @@ package game.findmyname;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,20 @@ public class acceuil extends AppCompatActivity {
         // Récup du mot dans l'activité précédente
         Intent intent = getIntent();
         String pseudo = intent.getStringExtra("pseudo");
+        final String id = intent.getStringExtra("id");
+
+        Log.i("ENI","id reçu : " +id);
+
+        TextView tvprofil = (TextView) findViewById(R.id.profil);
+        tvprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(acceuil.this,profil.class);
+                //Envoie de l'in vers la page de profil pour faire les requêtes de MAJ
+                intent.putExtra("id",id);
+                startActivity(intent);
+            }
+        });
 
         TextView tvpseudo = (TextView) findViewById(R.id.tvBonjour);
         tvpseudo.setText("Bonjour "+pseudo+" !");
@@ -33,11 +48,8 @@ public class acceuil extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 
-    public void gotoprofil(View v)
-    {
-        Intent intent = new Intent(acceuil.this,profil.class);
-        startActivity(intent);
-    }
 }
--- FindMyName-AndroidStudio/app/src/main/java/game/findmyname/profil.java
+++ FindMyName-AndroidStudio/app/src/main/java/game/findmyname/profil.java
@@ -1,7 +1,16 @@
 package game.findmyname;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+import data.findmyname.data;
 
 /**
  * Created by lucar_000 on 07/05/2016.
@@ -12,6 +21,45 @@ public class profil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profil);
+
+        // Récup de l'id de la page d'accueil
+        Intent intent = getIntent();
+        String strid = intent.getStringExtra("id");
+        Log.i("Profil" ,"id reçu : "+strid);
+
+        // Convertion de l'id en Int
+
+        int id = Integer.parseInt(strid);
+
+        EditText ModifPseudo = (EditText) findViewById(R.id.ModifMail);
+
+        EditText ModifMdp = (EditText) findViewById(R.id.ModifMDP);
+
+        EditText ModifMail = (EditText) findViewById(R.id.ModifMail);
+
+
+        data profildb = new data(getBaseContext(), "dbuser.db", null, 1);
+        final SQLiteDatabase db = profildb.getWritableDatabase();
+        db.setLocale(Locale.FRENCH);
+
+        Cursor result = db.rawQuery("SELECT * FROM user where id="+id ,null);
+
+        result.moveToFirst();
+
+        while (!result.isAfterLast())
+        {
+            String pseudodb = result.getString(1);
+            Log.i("Profil","pseudo : " +pseudodb);
+
+            String mdpdb = result.getString(2);
+            Log.i("Profil","mdp : " +mdpdb);
+
+
+            String maildb = result.getString(4);
+            Log.i("Profil","mail : " +maildb);
+
+            result.moveToNext();
+        }
 
     }
 }
