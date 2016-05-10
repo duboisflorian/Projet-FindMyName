0a6edf96a24174be47020186de0de86d1584f22a
Eldii <m1105.108.baba@gmail.com>
2016-05-10 16:24:35 +0200

    feat : utilisateur controller master

--- FindMyName-Serveur/src/FindMyName-Serveur/Controllers/UtilisateurController.cs
+++ FindMyName-Serveur/src/FindMyName-Serveur/Controllers/UtilisateurController.cs
@@ -17,98 +17,96 @@ namespace FindMyName_Serveur.Controllers
     [Route("api/[controller]")]
     public class UtilisateurController : Controller
     {
+        private IUtilisateurService _utilisateurService = new UtilisateurServiceEF();
 
-        private IUtilisateurService u = new UtilisateurServiceEF();
+        public UtilisateurController()
+        {
+            _utilisateurService = new UtilisateurServiceEF();
+        }
 
         [HttpGet]
-        public List<Utilisateur> Get()
+        public IList<Utilisateur> Get()
         {
-            return null;
+            return _utilisateurService.ALL();
         }
-        /*   // GET: api/values
-           [HttpGet]
-           public IEnumerable<Utilisateur> Get()
-           {
-               return UtilisateurService.UTILISATEURS;
-           }
 
-           // GET api/Utilisateur/{id}
-           [HttpGet("{id}")]
-           public Utilisateur Get(int id)
-           {
-               return UtilisateurService.getUser(id);
-           }
-           // GET api/Utilisateur/GetUserView/{id}
-           [HttpGet("GetUserView/{id}")]
-           public UtilisateurViewModel GetUserView(int id)
-           { 
-               //Contrôle des accès
+        // GET api/Utilisateur/{id}
+        [HttpGet("{id}")]
+        public Utilisateur Get(int id)
+        {
+            return _utilisateurService.getUser(id);
+        }
+        // GET api/Utilisateur/GetUserView/{id}
+        [HttpGet("GetUserView/{id}")]
+        public UtilisateurViewModel GetUserView(int id)
+        {
+            //Contrôle des accès
 
-               //Traitement
-               Utilisateur user = UtilisateurService.getUser(id);
+            //Traitement
+            Utilisateur user = _utilisateurService.getUser(id);
 
-               return new UtilisateurViewModel(user.id,user.name,user.photo);
-           }
+            return new UtilisateurViewModel(user.id, user.name, user.photo);
+        }
 
-           // GET api/verifMail/{mail}
-           [HttpGet("verifMail/{mail}")]
-           public Res verifMailExist(string mail)
-           {
-               if (UtilisateurService.verificationMailExist(mail) == true)
-                   return new Res("existe");
-               else
-                   return new Res("go");
-           }
+        // GET api/verifMail/{mail}
+        [HttpGet("verifMail/{mail}")]
+        public ResViewModel verifMailExist(string mail)
+        {
+            if (_utilisateurService.verificationMailExist(mail) == true)
+                return new ResViewModel("existe");
+            else
+                return new ResViewModel("go");
+        }
 
-           // GET api/verifConnexion/{mail}/{password}
-           [HttpGet("verifConnexion/{mail}/{password}")]
-           public Utilisateur verifConnexion(string mail, string password)
-           {
-               return UtilisateurService.verificationConnexion(mail, password);
-           }
+        // GET api/verifConnexion/{mail}/{password}
+        [HttpGet("verifConnexion/{mail}/{password}")]
+        public Utilisateur verifConnexion(string mail, string password)
+        {
+            return _utilisateurService.verificationConnexion(mail, password);
+        }
 
-           // GET api/sameMDP/{id}/{password}
-           [HttpGet("sameMDP/{id}/{password}")]
-           public Res sameMDP(int id, string password)
-           {
-               return new Res(UtilisateurService.Same_mdp(id, password));
-           }
+        // GET api/sameMDP/{id}/{password}
+        [HttpGet("sameMDP/{id}/{password}")]
+        public ResViewModel sameMDP(int id, string password)
+        {
+            return new ResViewModel(_utilisateurService.Same_mdp(id, password));
+        }
 
-           // GET api/ajoutus/{name}/{mail}/{password}
-           [HttpGet("ajoutus/{name}/{mail}/{password}")]
-           public Res ajoutus(string name, string mail, string password)
-           {
-               UtilisateurService.ajouterUtilisateur(name,mail,password);
-               return new Res("bon");
-           }
+        // GET api/ajoutus/{name}/{mail}/{password}
+        [HttpGet("ajoutus/{name}/{mail}/{password}")]
+        public ResViewModel ajoutus(string name, string mail, string password)
+        {
+            _utilisateurService.ajouterUtilisateur(name, mail, password);
+            return new ResViewModel("bon");
+        }
 
-           // GET api/getPhoto/{u}
-           [HttpGet("getPhoto/{u}")]
-           public Res getPhoto(int u)
-           {
-               return new Res(UtilisateurService.getPhoto(u));
-           }
+        // GET api/getPhoto/{u}
+        [HttpGet("getPhoto/{u}")]
+        public ResViewModel getPhoto(int u)
+        {
+            return new ResViewModel(_utilisateurService.getPhoto(u));
+        }
 
-           // GET api/getPays/{u}
-           [HttpGet("getPays/{u}")]
-           public  Res getPays(int u)
-           {
-               return new Res(UtilisateurService.getPays(u));
-           }
+        // GET api/getPays/{u}
+        [HttpGet("getPays/{u}")]
+        public ResViewModel getPays(int u)
+        {
+            return new ResViewModel(_utilisateurService.getPays(u));
+        }
 
-           // GET api/changePays/{id}/{selectPays}
-           [HttpGet("changePays/{id}/{selectPays}")]
-           public Res changePays(int id,string selectPays)
-           {
-              return new Res(UtilisateurService.changePays(id,selectPays));
-           }
+        // GET api/changePays/{id}/{selectPays}
+        [HttpGet("changePays/{id}/{selectPays}")]
+        public ResViewModel changePays(int id, string selectPays)
+        {
+            return new ResViewModel(_utilisateurService.changePays(id, selectPays));
+        }
 
-           // GET api/changePhoto/{id}/{selectPhoto}
-           [HttpGet("changePhoto/{id}/{selectPhoto}")]
-           public Res changePhoto(int id, string selectPhoto)
-           {
-              return new Res (UtilisateurService.changePhoto(id, selectPhoto));
-           }
-           */
+        // GET api/changePhoto/{id}/{selectPhoto}
+        [HttpGet("changePhoto/{id}/{selectPhoto}")]
+        public ResViewModel changePhoto(int id, string selectPhoto)
+        {
+            return new ResViewModel(_utilisateurService.changePhoto(id, selectPhoto));
+        }
+
     }
 }
