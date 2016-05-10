99dcb316bafb4626f0689679fefa0ea5051bbcbf
Eldii <m1105.108.baba@gmail.com>
2016-05-10 16:26:08 +0200

    feat : interface utilisateur master

--- FindMyName-Serveur/src/FindMyName-Serveur/Services/IUtilisateurService.cs
+++ FindMyName-Serveur/src/FindMyName-Serveur/Services/IUtilisateurService.cs
@@ -8,6 +8,8 @@ namespace FindMyName_Serveur.Interface
 {
     public interface IUtilisateurService
     {
+        IList<Utilisateur> ALL();
+        Utilisateur getUser(string name);
         Utilisateur getUser(int id);
         Utilisateur Existe(int id);
         Utilisateur verificationConnexion(string mail, string password);
@@ -16,22 +18,23 @@ namespace FindMyName_Serveur.Interface
         int getlastid();
         void ajouterUtilisateur(string name, string mail, string password);
         int getOnlineutilisateur();
-        string getName(int u);
-        void ChangerMeilleurScore(int u, int score);
-        string getPhoto(int u);
+        string getName(int id);
+        void ChangerMeilleurScore(int id, int score);
+        string getPhoto(int id);
         string changePhoto(int id, string selectPhoto);
-        string getPays(int u);
+        string getPays(int id);
         string changePays(int id, string selectPays);
-        ResID Jouer(int u);
+        ResIDViewModel Jouer(int id);
         Contact getAmiExiste(int id, int u);
         Boolean getOnlineExiste(int id, int u);
+        IList<Contact> ALLContact();
         void changerPhoto(int id, string photo);
         List<Contact> getContacts(int id, string type);
-        void addAmi(int user, int u);
-        void addOnline(int user, int u);
-        void creation(int id);
-        int getNbContact(int u);
-        void addAmiOnline(int user, int u);
-        String Add(int contactadd, int user, int u);
+        void addAmi(int user, int id);
+        void addOnline(int user, int id);
+        void creation(Utilisateur u1);
+        int getNbContact(int id);
+        void addAmiOnline(int user, int id);
+        String Add(int contactadd, int user, int id);
     }
 }
